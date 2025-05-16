c895f0569ae26e540930094b3b8fb19ae7bc07b7
shafinulh <haqueshafinul@gmail.com>
2025-05-15 23:38:22 -0400

    flink and rocksdb access latency visualization

--- flink_java_setup/rocksdb-options/src/main/java/com/example/CustomRocksDBOptionsFactory.java
+++ flink_java_setup/rocksdb-options/src/main/java/com/example/CustomRocksDBOptionsFactory.java
@@ -5,6 +5,7 @@ import org.apache.flink.contrib.streaming.state.RocksDBOptionsFactory;
 import org.apache.flink.contrib.streaming.state.RocksDBNativeMetricOptions;
 import org.rocksdb.*;
 
+import java.io.File;
 import java.lang.reflect.Field;
 import java.util.Collection;
 import java.util.Set;
@@ -14,25 +15,42 @@ public class CustomRocksDBOptionsFactory implements RocksDBOptionsFactory {
     
     // Memory configuration constants
     private static final double HIGH_PRIORITY_POOL_RATIO = 0.1;
-    private static final long BLOCK_CACHE_SIZE = 107 * 1024 * 1024; 
-    private static final long WRITE_BUFFER_SIZE = 64 * 1024 * 1024;
-    private static final long TOTAL_WRITE_BUFFER_LIMIT = 170 * 1024 * 1024;
+    private static final long WRITE_BUFFER_SIZE = 32 * 1024 * 1024;
+
+    private static final long BLOCK_CACHE_SIZE = 2 * 1024 * 1024;
+    private static final long TOTAL_WRITE_BUFFER_LIMIT = 24 * 1024 * 1024;
+    private static final int BLOCK_CACHE_SHARD_BITS = 1;
+
+    // stat dumps to see histogram types - turn off when not needed
+    // private static final int STATS_DUMP_PERIOD_SEC = 30; // Dump stats every 90 seconds
+    // private static final String ROCKSDB_LOG_SUBDIR_NAME = "rocksdb_native_logs"; // Subdirectory for these logs
 
     @Override
     public DBOptions createDBOptions(DBOptions currentOptions, Collection<AutoCloseable> handlesToClose) {
-        Cache cache = new LRUCache(BLOCK_CACHE_SIZE, 4, false, HIGH_PRIORITY_POOL_RATIO);
+        Cache cache = new LRUCache(BLOCK_CACHE_SIZE, BLOCK_CACHE_SHARD_BITS, false, HIGH_PRIORITY_POOL_RATIO);
         handlesToClose.add(cache);
 
-        WriteBufferManager writeBufferManager = new WriteBufferManager(TOTAL_WRITE_BUFFER_LIMIT, cache);
+        WriteBufferManager writeBufferManager = new WriteBufferManager(TOTAL_WRITE_BUFFER_LIMIT, new LRUCache(1));
         handlesToClose.add(writeBufferManager);
 
         Statistics statistics = new Statistics();
         statistics.setStatsLevel(StatsLevel.ALL);
         handlesToClose.add(statistics);
 
+        // currentOptions.setStatsDumpPeriodSec(STATS_DUMP_PERIOD_SEC);
+        // String flinkLogDirEnv = System.getenv("FLINK_LOG_DIR");
+        // String baseLogDir = (flinkLogDirEnv != null && !flinkLogDirEnv.isEmpty()) ? flinkLogDirEnv : ".";
+        // String rocksDbInstanceLogPath = baseLogDir + File.separator + ROCKSDB_LOG_SUBDIR_NAME;
+        // File rocksDbLogDirFile = new File(rocksDbInstanceLogPath);
+        // if (!rocksDbLogDirFile.exists()) {
+        //     rocksDbLogDirFile.mkdirs();
+        // }
+        // currentOptions.setDbLogDir(rocksDbInstanceLogPath);
+
         return currentOptions
-                .setTableCacheNumshardbits(4)
+                .setTableCacheNumshardbits(BLOCK_CACHE_SHARD_BITS)
                 .setWriteBufferManager(writeBufferManager)
+                .setUseDirectReads(true)
                 .setStatistics(statistics);
     }
 
@@ -78,10 +96,12 @@ public class CustomRocksDBOptionsFactory implements RocksDBOptionsFactory {
                 .setCacheIndexAndFilterBlocks(false)
                 .setCacheIndexAndFilterBlocksWithHighPriority(false)
                 .setPinL0FilterAndIndexBlocksInCache(false)
+                .setPinTopLevelIndexAndFilter(false)
                 .setBlockCache(cache);
 
         return currentOptions
                 .setWriteBufferSize(WRITE_BUFFER_SIZE)
+                .setMaxWriteBufferNumber(2)
                 .setTableFormatConfig(tableConfig);
         // return currentOptions;
     }
